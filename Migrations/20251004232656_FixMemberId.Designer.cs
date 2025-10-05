55a9574412613451e77d3cffe289f62b3272918f
TJ Fisher <tjfisher2015@gmail.com>
2025-10-05 08:39:34 -0600

    Add self-referencing MemberId to Members table
    Introduced a nullable `MemberId` column in the `Members` table to enable self-referencing relationships (e.g., spouses).

--- Migrations/20251004232656_FixMemberId.Designer.cs
+++ Migrations/20251004232656_FixMemberId.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using FamilyArchiveBackend.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace FamilyArchiveBackend.Migrations
 {
     [DbContext(typeof(FamilyArchiveContext))]
-    partial class FamilyArchiveContextModelSnapshot : ModelSnapshot
+    [Migration("20251004232656_FixMemberId")]
+    partial class FixMemberId
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20251004232656_FixMemberId.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FamilyArchiveBackend.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixMemberId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MemberId",
+                table: "Members",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Members_MemberId",
+                table: "Members",
+                column: "MemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Members_Members_MemberId",
+                table: "Members",
+                column: "MemberId",
+                principalTable: "Members",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Members_Members_MemberId",
+                table: "Members");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Members_MemberId",
+                table: "Members");
+
+            migrationBuilder.DropColumn(
+                name: "MemberId",
+                table: "Members");
+        }
+    }
+}
